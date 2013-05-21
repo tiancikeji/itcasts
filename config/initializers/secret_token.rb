c9f149c200106d44150bc64d393ae2d2bf1bb898
王雷 <wanglei@chinacache.com>
2013-05-21 13:27:14 +0800

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itcasts::Application.config.secret_token = '247d9768953e243650bc40d87c6a908a9b4a9602ffd165babfa0667feb227b5ea6b2243e7927d50a7de090bdb13dd924f0af584e8c009015877a3932b320b43f'
